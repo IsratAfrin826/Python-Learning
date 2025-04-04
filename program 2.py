dea6186a063389afce9777bfd7e49ed9262fdd19
Israt Arefin <nisratafrin826@gmail.com>
2025-04-04 20:27:39 +0600

    Add files via upload

---
+++ program 2.py
@@ -0,0 +1,6 @@
+name ="Israt Arefin"
+age =22
+print("Our new stuent name is " + name)
+print(name +  " lives in Chittagong")
+print("she is currently" , age , "years old" )
+print("At the age of" , age , "she has started to learn python")
