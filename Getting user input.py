dd518c4f16385369cc6b9abaa200baa1bb1ba766
Israt Arefin <nisratafrin826@gmail.com>
2025-04-14 12:20:01 +0600

    Add files via upload

---
+++ Getting user input.py
@@ -0,0 +1,9 @@
+name= input("Enter your name : ")
+age =input("Enter your age : ")
+cgpa=input("Enter your cgpa : ")
+
+print("Student Information")
+print("______________________")
+print("Name :"+name)
+print("Age :"+age)
+print("cgpa :"+cgpa)
