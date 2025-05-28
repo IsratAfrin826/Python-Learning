e25dc564f8837e8521bdc3b09fc2ddea327ea6da
Mosammat Israt Arefin Mazumder <nisratafrin826@gmail.com>
2025-05-28 23:21:26 +0600

    Add files via upload

---
+++ List part 1.py
@@ -0,0 +1,10 @@
+
+subjects = ["C" , "C++" , "Python" , "java" ]
+
+print (subjects)
+print(subjects[0])
+print(subjects[2:])
+print(subjects[-1])
+print("Python" in subjects)
+print("HTML" not in subjects)
+print(subjects + ["HTML"])
