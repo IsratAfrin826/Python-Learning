e5df544b9edddc2f99464775bc4a326c4dd4b627
Mosammat Israt Arefin Mazumder <nisratafrin826@gmail.com>
2025-05-28 13:16:28 +0600

    Add files via upload

---
+++ if , else statement.py
@@ -0,0 +1,25 @@
+#pass/fail
+
+mark =80
+
+if mark>=33:
+    print("pass")
+else:
+    print("fail")
+
+#largest
+num1 = 25
+num2 =13
+
+if num1>num2:
+    print(num1)
+else:
+    print(num2)
+
+#even/odd
+num =4
+
+if num % 2==0:
+    print ("Even")
+else:
+    print("Odd")
