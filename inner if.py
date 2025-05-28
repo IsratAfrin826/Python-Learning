59665458ba1be3edf0e8a074d8e456c6ef486973
Mosammat Israt Arefin Mazumder <nisratafrin826@gmail.com>
2025-05-28 13:17:24 +0600

    Add files via upload

---
+++ inner if.py
@@ -0,0 +1,15 @@
+num1 = 67
+num2 = 40
+num3 = 58
+
+if num1 > num2:
+    if num1 > num3:
+        print(num1)
+    else:
+        print(num3)
+
+else:
+    if num2 > num3:
+        print(num2)
+    else:
+        print(num3)
