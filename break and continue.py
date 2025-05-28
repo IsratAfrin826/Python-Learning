5d5ec6bfe73b6f5a2ac5fc4564c165d6ba1d5cfd
Mosammat Israt Arefin Mazumder <nisratafrin826@gmail.com>
2025-05-28 23:20:29 +0600

    Add files via upload

---
+++ break and continue.py
@@ -0,0 +1,18 @@
+# break
+i = 1
+while i <= 100:
+    print(i)
+    i = i + 1
+    if i == 20:
+        break
+print ("End")
+
+# continue
+
+i = 1
+while i <= 100:
+    print(i)
+    i = i + 1
+    if i == 20:
+        continue
+print ("End")
