0d87c42fb9e878bf76ce937853738fd15d452eff
Mosammat Israt Arefin Mazumder <nisratafrin826@gmail.com>
2025-05-30 22:49:49 +0600

    Add files via upload

---
+++ dictionaries.py
@@ -0,0 +1,10 @@
+
+studentId = {
+    101 : "Israt",
+    102 : "Asma",
+    103 : "Sathi",
+    104 : "Nitu",
+    105 : "Habiba",
+}
+print(studentId.get(103,"Not a valid key"))
+print(studentId.get(106,"Not a valid key"))
