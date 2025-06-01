97437202f310e346648093b199f4edd26a207b6b
Mosammat Israt Arefin Mazumder <nisratafrin826@gmail.com>
2025-06-01 11:27:56 +0600

    Add files via upload

---
+++ function.py
@@ -0,0 +1,23 @@
+
+def add(x,y):
+    sum = x + y
+    print(sum)
+
+
+def sub(x,y):
+    sub = x - y
+    print(sub)
+
+
+def mul(x,y):
+    mul = x * y
+    print(mul)
+
+
+def message():
+    print("No parameter")
+
+add(20,30)
+sub(50,20)
+mul(20,3)
+message()
