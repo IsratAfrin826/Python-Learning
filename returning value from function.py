757d155175e8471262e5edf2c3986b67341a75bd
Mosammat Israt Arefin Mazumder <nisratafrin826@gmail.com>
2025-06-03 11:37:01 +0600

    Add files via upload

---
+++ returning value from function.py
@@ -0,0 +1,30 @@
+
+def add(a,b):
+    sum = a + b
+    return sum
+
+result = add(30,40)
+print("Result = ",result)
+
+def large(a,b):
+    if a > b:
+        return a
+    else:
+        return b
+
+result = large(30, 40)
+print("Large number is = ", result)
+
+
+def large(a,b):
+    if a > b:
+        return a
+    else:
+        return b
+
+maximum = large
+print(maximum(100,234 ))
+
+
+
+
