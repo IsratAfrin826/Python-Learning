fd8176da4a3f63c3bc7f6ff26d88dfb082edf9eb
Mosammat Israt Arefin Mazumder <nisratafrin826@gmail.com>
2025-06-06 21:26:48 +0600

    Add files via upload

---
+++ class & object.py
@@ -0,0 +1,14 @@
+class student:
+    roll = " "
+    gpa = ""
+
+
+Israt = student()
+Israt.roll = 101
+Israt.gpa = 3.90
+print (f"Roll : {Israt.roll},GPA : {Israt.gpa}")
+
+Asma = student()
+Asma.roll = 102
+Asma.gpa = 3.95
+print (f"Roll : {Asma.roll},GPA : {Asma.gpa}")
