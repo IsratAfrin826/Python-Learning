af65bd034040e724a3bb88c63e2be72367ee1abc
Mosammat Israt Arefin Mazumder <nisratafrin826@gmail.com>
2025-06-08 12:48:15 +0600

    Add files via upload

---
+++ polymorphism.py
@@ -0,0 +1,11 @@
+# Built in polymorphism function
+print(len("Israt"))
+print(len([10,20,30]))
+
+#user defined polymorphism function
+def add(x,y,z=0):
+    return x + y + z
+
+
+print(add(2,3))
+print(add(2,3,4))
